9b496f7c9bd3c082d909f01047f4877baa9ed6fd
Mukarram Baig <baigm@deshaw.com>
2013-07-24 11:37:35 +0530

    Working version!

--- proguard-tester-parent/proguard-tester-client/src/test/java/com/kilo/SpecialServiceJaxRSTest.java
+++ proguard-tester-parent/proguard-tester-client/src/test/java/com/kilo/SpecialServiceJaxRSTest.java
@@ -4,7 +4,6 @@ package com.kilo;
 import static org.junit.Assert.assertNotNull;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import javax.annotation.Resource;

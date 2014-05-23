b559f3efd0084fa76a51cde3fc7a32c57b5be3c1
Mukarram Baig <baigm@deshaw.com>
2014-05-23 10:38:50 +0530

    Spring 4 move, java 8 and proguard 5.0beta1
    Update compiler version from 2.3.2 to 3.1 and source-target to 1.8
    Use BOM-POM from springframework for better dependencyManagement
    Modified driver to be auto closeable
    cxf to 2.7.11

--- proguard-tester-parent/proguard-tester-business/src/main/java/com/kilo/ProguardBusinessDriver.java
+++ proguard-tester-parent/proguard-tester-business/src/main/java/com/kilo/ProguardBusinessDriver.java
@@ -2,17 +2,21 @@
 package com.kilo;
 
 import org.apache.log4j.Logger;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ProguardBusinessDriver {
 
     private static Logger LOG = Logger.getLogger(ProguardBusinessDriver.class);
 
-    public static void main(String[] args) {
-        new ClassPathXmlApplicationContext(
+    public static void main(String[] args) throws InterruptedException {
+        try (ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext(
                 "classpath:com/kilo/proguard/logging.xml",
-                "classpath:com/kilo/proguard/applicationContext.xml");
-        LOG.info("Inited");
+                "classpath:com/kilo/proguard/applicationContext.xml")) {
+            LOG.info("Inited");
+            // sleep for an hour while we do some playing around
+            Thread.sleep(60 * 60 * 1_000);
+        }
     }
 
 }
